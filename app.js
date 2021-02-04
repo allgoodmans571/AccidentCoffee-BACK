b632278f0d6d25c57b6e09143593f394055e8861
Alex <allgoodmans571@gmail.com>
2021-02-04 20:04:00 +0300

    cors

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const jsonParser = bodyParser.json();
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
