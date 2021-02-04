fb7528885e1fca2f74c34afd402fb0a10717c94b
Alex <allgoodmans571@gmail.com>
2021-02-04 05:03:12 +0300

    registration component development

--- app.js
+++ app.js
@@ -4,41 +4,67 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const User = require("./models/User");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const jsonParser = bodyParser.json();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 mongoose.connect(
   "mongodb+srv://alex:password15326@dev.ffuml.mongodb.net/dev?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 );
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.get("/", (req, res) => {
   res.send({ status: "OK" });
 });
 
-app.get("/mng", (req, res) => {
-  let usr = new User({
+app.post("/mng", (req, res) => {
+  User.create({
     position: "Programmer",
     name: "Alex",
     email: "alex1@alex.com",
   });
-  try {
-    usr.save();
-  } catch (error) {
-    console.error(error);
-  }
+
   console.log("Saved");
 });
 
-app.get("/test", (req, res) => {
+app.post("/test", (req, res) => {
   User.findOne({ email: "maxim@max.com" }, "name email", function (err, user) {
     if (err) return console.error(err);
     try {
       console.log(user.name);
+      res.send(user.name);
     } catch (err) {
       console.error(err);
+      res.send(err);
     }
   });
+});
+
+app.post("/registration", jsonParser, (req, res) => {
+  body = req.body;
+  User.create({
+    position: body.position,
+    name: body.name,
+    email: body.email,
+  })
+    .then(() => {
+      res.status(200);
+      console.log("saved");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ status: 400 });
+    });
 });
 
 app.listen(8080);
--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-  position: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  position: { type: String, required: true },
+  
 });
 
 module.exports = mongoose.model("User", schema);
